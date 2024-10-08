435af636da0ed656e454999bec1e21f54ce2866e
Carter <65513082+WalterWQ@users.noreply.github.com>
2024-10-08 01:17:29 +0100

    Create entrypoint.sh

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+function updateGame() {
+    steamcmd \
+        +login ${USERNAME} ${PASSWRD} \
+        +force_install_dir ${SERVER_DIR} \
+        +app_update ${GAME_ID} \
+        +quit
+}
+
+function startGame() {
+    cd ${SERVER_DIR}
+    ./DayZServer \
+        -config="serverDZ.cfg" 
+}
+
+case "$1" in
+    start)
+        updateGame
+        startGame
+    ;;
+    update)
+        updateGame
+    ;;
+    *)
+        exec "$@"
+    ;;
+esac
